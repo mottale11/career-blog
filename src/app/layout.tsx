6784dc8bb232c8d6041915bdd0e35a5c7540591c
mottale11 <mosesmwai100@gmail.com>
2026-01-31 11:16:02 +0300

    Add Google Tag Manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,12 +39,31 @@ export default function RootLayout({
           crossOrigin="anonymous"
         />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet" />
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-K485M6RK');
+        `}
+        </Script>
       </head>
       <body
         className={cn(
           'min-h-screen bg-background font-body antialiased flex flex-col'
         )}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K485M6RK"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-YEJ7E7K9LC"
           strategy="afterInteractive"
